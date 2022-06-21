c65a46b0b5c9522b0ba63685d10861b9b97f4a3d
an196 <an9x0010@gmail.com>
2022-06-21 11:00:33 +0700

    config authentication

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConnection');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
+
+// custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+app.use('/users', require('./routes/api/users'));
+
+app.use(verifyJWT);
+
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
